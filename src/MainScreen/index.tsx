3446d6b4c1fb5c1b50faf11dc274400d98c636d6
Dmitry Tsyplov <dmitry.tsyplov@cibirlan.com>
2022-09-13 17:24:08 +0200

    add modal

--- src/MainScreen/index.tsx
+++ src/MainScreen/index.tsx
@@ -1,55 +1,215 @@
 import React, {useState} from 'react';
-import Basket from '../assets/SVG/Basket.svg';
+import Basket from '../assets/SVG/trash-969.svg';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  FlatList,
   View,
+  Modal,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
+const itemData = [
+  {subject: 'math', task: 'Стр 4, упр. 24'},
+  {subject: 'math', task: 'Стр 4, упр. 24'},
+];
 
 const MainScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const [isSelected, setSelection] = useState(false);
+  const RenderItem = () => {
+    return (
+      <>
+        <View style={styles.itemStyle}>
+          <View style={styles.leftWrapper}>
+            <View style={styles.checkBoxWrapper}>
+              <CheckBox
+                lineWidth={1}
+                animationDuration={0.5}
+                onCheckColor={'#fff'}
+                onFillColor={'#19C149'}
+                tintColors={{true: '#000'}}
+                onTintColor={'#19C149'}
+                disabled={false}
+                onAnimationType={'bounce'}
+                offAnimationType={'bounce'}
+                boxType={'square'}
+                value={isSelected}
+                onValueChange={setSelection}
+              />
+            </View>
+            <View style={styles.subjectWrapper}>
+              <Text style={styles.title}>Матем</Text>
+              <Text style={styles.text}>Стр 4, упр. 24</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity style={styles.iconStyle}>
+            <View style={styles.wrapperStyle}>
+              <Basket width={18} height={18} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.lowLineStyle} />
+      </>
+    );
+  };
   return (
     <SafeAreaView>
       <StatusBar barStyle={'light-content'} />
+      <TouchableOpacity style={styles.buttonStyle}>
+        <Text style={styles.textButtonStyle}>Показывать все задания</Text>
+      </TouchableOpacity>
+      <View style={styles.berderLineStyle} />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.textButtonStyle}>Показывать все задания</Text>
-        </TouchableOpacity>
-        <View style={styles.berderLineStyle} />
-        <View style={styles.itemStyle}>
-          <View style={styles.checkBoxWrapper}>
-            <CheckBox
-              lineWidth={1}
-              animationDuration={0.5}
-              onCheckColor={'#fff'}
-              onFillColor={'#19C149'}
-              tintColors={{true: '#000'}}
-              onTintColor={'#19C149'}
-              disabled={false}
-              onAnimationType={'bounce'}
-              offAnimationType={'bounce'}
-              boxType={'square'}
-              value={isSelected}
-              onValueChange={setSelection}
-            />
-          </View>
-          <View style={styles.subjectWrapper}>
-            <Text style={styles.title}>Матем</Text>
-            <Text style={styles.text}>Стр 4, упр. 24</Text>
-          </View>
-          <Basket />
-        </View>
+        <FlatList
+          nestedScrollEnabled
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          data={itemData}
+          renderItem={RenderItem}
+        />
       </ScrollView>
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        style={styles.addButtonStyle}>
+        <Text style={styles.addTextButtonStyle}>Добавить</Text>
+      </TouchableOpacity>
+
+      <Modal
+        style={styles.modalWrapper}
+        animationType="fade"
+        transparent
+        visible={modalVisible}>
+        <View>
+          <View style={styles.modal}>
+            <View style={styles.modalBlock}>
+              <Text style={styles.modalText}>Добавить предмет</Text>
+              <Text style={styles.modalsubText}>
+                Укажите заголовок и задание
+              </Text>
+              <TextInput
+                placeholder="Заголовок"
+                style={styles.input}
+                onChangeText={() => console.log('YYYY')}
+                // value={''}
+              />
+              <TextInput
+                placeholder="Задание"
+                style={styles.input}
+                onChangeText={() => console.log('YYYY')}
+                // value={''}
+              />
+
+              <View style={styles.lowButtonWrapper}>
+                <TouchableOpacity onPress={() => setModalVisible(false)}>
+                  <Text>Отмена</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setModalVisible(false)}>
+                  <Text>Сохранить</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  lowButtonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '70%',
+    alignSelf: 'center',
+    marginBottom: 10,
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    padding: 10,
+  },
+  modalText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 6,
+  },
+  modalsubText: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 6,
+  },
+  modalBlock: {
+    paddingTop: 10,
+    width: '80%',
+
+    backgroundColor: 'rgba(255,255,255,0.6)',
+    borderRadius: 8,
+  },
+  modal: {
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    borderRadius: 14,
+    padding: 24,
+  },
+
+  modalWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+  addButtonStyle: {
+    width: '80%',
+    backgroundColor: '#3396FF',
+
+    borderRadius: 8,
+    alignSelf: 'center',
+    paddingBottom: 8,
+    paddingTop: 8,
+    marginBottom: 40,
+  },
+  addTextButtonStyle: {
+    color: '#fFF',
+    alignSelf: 'center',
+    fontSize: 16,
+  },
+  lowLineStyle: {
+    width: '90%',
+    height: 1,
+    alignSelf: 'center',
+    backgroundColor: '#B9BDC4',
+    marginBottom: 16,
+  },
+  leftWrapper: {
+    flexDirection: 'row',
+  },
+  wrapperStyle: {
+    alignSelf: 'center',
+  },
+  iconStyle: {
+    alignSelf: 'center',
+    width: 32,
+    height: 32,
+    backgroundColor: '#F7F6F6',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -59,7 +219,7 @@ const styles = StyleSheet.create({
   },
   subjectWrapper: {
     width: 150,
-    marginLeft: 20,
+    marginLeft: 26,
     alignSelf: 'center',
   },
   checkBoxWrapper: {
@@ -71,6 +231,8 @@ const styles = StyleSheet.create({
     width: '90%',
     flexDirection: 'row',
     alignSelf: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 16,
   },
   berderLineStyle: {
     width: '100%',
